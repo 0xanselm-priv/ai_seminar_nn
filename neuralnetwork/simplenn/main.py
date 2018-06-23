fc40d3008360495aea29ab0982afc6f7923e9843
R <R@DESKTOP-3U6BLH4>
2018-06-23 22:27:35 +0200

    restructred a few things

--- neuralnetwork/simplenn/main.py
+++ neuralnetwork/simplenn/main.py
@@ -19,200 +19,25 @@ values are rounded by default. Internally they are stored as 16bit float.
 we can certainly expand to 32 or 64 bit or even 128.
 """
 
-import numpy as np
-import math
-import random
-import sys
-#import simplenn
-#from simplenn import network
-#from simplenn import node
-import network_robert
-from tkinter import *
-import itertools
-
 __author__ = "Niels #1, Niels #2"
 __copyright__ = "Copyright 2017/2018 – EPR-Goethe-Uni"
 __credits__ = "If you would like to thank somebody \
               i.e. an other student for her code or leave it out"
-__email__ = "niels.heissel@stud.uni-frankfurt.de"
+__email__ = ""
+
+import network_class
 
 
 class Main():
-    input_vector_list = []
-    target_vector_list = []
-    data_list = []
 
     def __init__(self):
         self.main()
 
     def main(self):
-        #         first_network = network.Network([[node.Node(1, 0), node.Node(2, 1)], [node.Node(0, 0)], [
-        #             node.Node(0, 0), node.Node(5, 1), node.Node(4, 2)]])
-        #         first_Network.create_network()
-        #         first_network.create_network_randomly()
-        #         nn_conf = [(1,4),(2,3),(3,4),(4,5),(5,2)] # Paper NN config
-        #         control errything from here
+        a = network_class.Network([(3), (2), (2)], activation_function="sigmoid", initilizer="xavier_sigmoid")
+        a.test_info([[3], [2], [2]], [[2], [2]])
 
-        self.data_fetch()
-        self.construct_input_vector()
-        self.construct_target_vector_list()
-#         theese tuples are the structure of the network
-#         each tuple represents a layer
-#         the first entry is the layer number, starting with 1
-#         because layer 0 is the input layer
-#         and the second number describes the number of nodes/neurons
-#         if you want to adjust the network just build a new tuple collection
-
-        b = np.array(0, np.float16)
-        b.resize((2, 1))
-        x1 = 1
-        x2 = 0
-        b.itemset((0, 0), x1)
-        b.itemset((1, 0), x2)
-
-        nn_conf_2x2 = [(1, 2), (2, 3), (3, 1)]
-        # <----- could run with "relu". buggy rn
-        a = network_robert.Network(nn_conf_2x2, "sigmoid")
-
-
-        for i in random.sample(list(itertools.product(range(-100, 100), range(-100, 100))), 5000):
-
-            x = np.array([[i[0]/100],[i[1]/100]])
-            y = np.array([[np.linalg.norm(x)]])
-            print(y)
-            a.train_nn(x, y)
-
-        a.test(np.array([[3/1000],[2/1000]]))
-
-        img_list = np.full((1000, 1000), '#ffffff')
-
-        #self.draw_image(img_list)
-
-        for i in range(100):
-            for j in range(100):
-                x = np.array([[i/100], [j/100]])
-                img_list[i][j] = self.color_set(a.test(x)[0])
-
-        self.draw_image(img_list)
-
-
-
-        a.nn_information()
-
-
-
-########## Deutschland Karte Training und Testdaten
-        # a.test(self.input_vector_list[17])
-        # a.test(self.input_vector_list[1002])
-        # a.test(self.input_vector_list[102])
-        # a.test(self.input_vector_list[20000])
-        # a.test(self.input_vector_list[999])
-        # a.test(np.array([[0.9],[0.9]]))
-        #
-        # a.train_nn(b, np.array([[1],[0]]))
-        #
-        #
-        # for i in random.sample(range(len(self.input_vector_list)), 2000):
-        #     print(self.target_vector_list[i])
-        #     print(self.input_vector_list[i])
-        #     a.train_nn(self.input_vector_list[i], self.target_vector_list[i])
-        #
-        #
-        #
-        # a.test(self.input_vector_list[17])
-        # a.test(self.input_vector_list[1002])
-        # a.test(self.input_vector_list[102])
-        # a.test(self.input_vector_list[20000])
-        # a.test(self.input_vector_list[999])
-        #
-        # a.nn_information()
-        #
-        #
-        # img_list = np.full((171, 207), '#ffffff')
-        #
-        # #self.draw_image(img_list)
-        #
-        # counter = 0
-        # print(a.test(self.input_vector_list[2]))
-        # for i in range(171):
-        #     for j in range(207):
-        #         print(self.input_vector_list[counter])
-        #         img_list[i][j] = self.color_set(a.test(self.input_vector_list[counter])[1])
-        #         counter += 1
-        # self.draw_image(img_list)
-
-###################
-
-        # j = 0
-        # for i in self.input_vector_list:
-        #     a.apply_input(i)n
-        #     a.target_vector_constructor(self.target_vector_list[j])
-        #     a.cost_function()
-        #     j += 1
-        # print(a.nn_cost())
-
-    def data_fetch(self):
-        fp_data = "test_data.txt"
-        with open(fp_data, "r") as f:
-            file_data = f.read()
-        f.closed
-        file_data = file_data.split(";")
-        list_temp = []
-        for i in range(len(file_data) - 1):
-            separated = file_data[i].split("_")
-            # m n category
-            a = (separated[0], separated[1], separated[2])
-            list_temp.append(a)
-        self.data_list = list_temp
-
-    def construct_input_vector(self):
-        for i in self.data_list:
-            a = np.array(0, np.float16)
-            a.resize((2, 1))
-            x1 = int(i[0]) / 171
-            x2 = int(i[1]) / 207
-            a.itemset((0, 0), x1)
-            a.itemset((1, 0), x2)
-            self.input_vector_list.append(a)
-
-    def construct_target_vector_list(self):
-        for i in self.data_list:
-            a = np.array(0, np.float16)
-            a.resize((2, 1))
-            if i[2] == "0":
-
-                a.itemset((0, 0), 1)
-                a.itemset((1, 0), 0)
-                self.target_vector_list.append(a)
-            elif i[2] == "1":
-                x1 = i[0]
-                x2 = i[1]
-                a.itemset((0, 0), 0)
-                a.itemset((1, 0), 1)
-                self.target_vector_list.append(a)
-
-    def greyscale_in_rgb(self, greyscale_black):
-        return (255 - 255 * greyscale_black, 255 - 255 * greyscale_black, 255 - 255 * greyscale_black)
-
-    def rgb_in_hex(self_, rgb):
-         return "#%02x%02x%02x" % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
-
-    def color_set(self, greyscale):
-        return self.rgb_in_hex(self.greyscale_in_rgb(greyscale))
-
-    def draw_image(self, img_list):
-        A=Tk()
-        B=Canvas(A)
-        B.place(x=0,y=0,height=100,width=100)
-        for a in range(100):
-            for b in range(100):
-                B.create_line(a,b,a+1,b+1,fill=img_list[a][b])  # where pyList is a matrix of hexadecimal strings
-        A.geometry("171x207")
-        mainloop()
-
-
+        
 if __name__ == "__main__":
     Main()
-
-
 
--- neuralnetwork/simplenn/network_class.py
+++ neuralnetwork/simplenn/network_class.py
@@ -1,12 +1,13 @@
 import numpy as np
-import random
-import sys
 import math
+import sys
 
 
 class Network:
-    def __init__(self, layer_infos, activation_function, weights = [], bias = []):
+
+    def __init__(self, layer_infos, activation_function="sigmoid", weights=[], bias=[], initilizer="random"):
         self.activation_function = activation_function
+        self.initializer = initilizer
         self.layer_infos = layer_infos
         self.layer_number = len(layer_infos)
         self.weights = []
@@ -14,40 +15,57 @@ class Network:
         self.activation = []
         self.output = 0
         self.target = 0
-
-        self.layer_init(layer_infos, weights, bias)
+        
+        self.initializers()
+        # self.layer_init(layer_infos, weights, bias)
         self.print_nn_info()
-
-
-
-
-
-    def layer_init(self, layer_infos, weights, bias):
-        if type(weights) == np.matrixlib.defmatrix.matrix and type(bias) == np.matrixlib.defmatrix.matrix:  # if weights are matrices
-            for layer_num in range(self.layer_number):
-                # check wether weight-matrix is right shape
-                if weights[layer_num].shape == (layer_infos[layer_num], layer_infos[layer_num + 1]):
-                    self.weights.append(weights[layer_num])
-                else:
-                    print("Sorry wrong dimension for your weight-matrix: Dimension ", weights[layer_num].shape)
-                if bias[layer_num].shape == (layer_infos[layer_num], 1):
-                    self.bias.append(bias[layer_num])
-                else:
-                    print("Sorry wrong dimension for your bias-matrix: Dimension ", bias[layer_num].shape)
-
-        elif weights == "xavier":
-            return None  # here you go mate
+    
+    def print_initilizer(self):
+        if self.initializer == "random":
+            return("Random")
+        elif self.initializer == "xavier_sigmoid":
+            return("Xavier Sigmoid")
         else:
-            print("You choose to randomly assign weights and bias.")
+            print("Wrong Initializer")
+            sys.exit()
+    
+    def initializers(self):
+        if self.initializer == "random":            
             for layer_num in range(self.layer_number - 1):
-                self.weights.append(np.matrix(np.random.randint(5, size=(layer_infos[layer_num + 1], layer_infos[layer_num]))))
-
+                self.weights.append(np.matrix(np.random.randint(5, size=(self.layer_infos[layer_num + 1], self.layer_infos[layer_num]))))
+                self.bias.append(np.matrix(np.random.randint(5, size=(self.layer_infos[layer_num + 1], 1))))
+        elif self.initializer == "xavier_sigmoid":
             for layer_num in range(self.layer_number - 1):
-                self.bias.append(np.matrix(np.random.randint(5, size=(layer_infos[layer_num + 1], 1))))
-
+                # TO DO
+                self.weights.append(np.matrix(np.random.randint(5, size=(self.layer_infos[layer_num + 1], self.layer_infos[layer_num]))))
+                self.bias.append(np.matrix(np.random.randint(5, size=(self.layer_infos[layer_num + 1], 1))))
+        for i in self.weights:
+            print(i.shape)
+        
+#     def layer_init(self, layer_infos, weights, bias):
+#         if type(weights) == np.matrixlib.defmatrix.matrix and type(bias) == np.matrixlib.defmatrix.matrix:  # if weights are matrices
+#             for layer_num in range(self.layer_number):
+#                 # check whether weight-matrix is right shape
+#                 if weights[layer_num].shape == (layer_infos[layer_num], layer_infos[layer_num + 1]):
+#                     self.weights.append(weights[layer_num])
+#                 else:
+#                     print("Error in weight-matrix: Dimension ", weights[layer_num].shape)
+#                 if bias[layer_num].shape == (layer_infos[layer_num], 1):
+#                     self.bias.append(bias[layer_num])
+#                 else:
+#                     print("Error in bias-matrix: Dimension ", bias[layer_num].shape)
+# 
+#         elif isinstance(weights, str):
+#             self.initializers(weights)
+#         
+#         else:
+#             print("Randomly initialized weights and biases.")
+#             pass
 
     def print_nn_info(self):
         print("A CNN with " + str(self.layer_number) + " layers.")
+        print("Activation Function used:", self.activation_function)
+        print("Initializer used:", self.print_initilizer())
         for layer_num in range(self.layer_number):
             print("Layer", str(layer_num), "with", self.layer_infos[layer_num], "nodes")
 
@@ -57,8 +75,7 @@ class Network:
                 str(i) + " -> " + str(i + 1) + " Matrix " + "\n"
             bias_str = "Layer " + str(i + 1) + " Bias Vector." + "\n"
             print(layer_str, self.weights[i])
-            print(bias_str, self.bias[i])
-        print("Activation Function used:", self.activation_function)
+            print(bias_str, self.bias[i])        
         print("Input Size:", self.layer_infos[0], "x 1")
 
     def activate(self, inp):
@@ -67,10 +84,14 @@ class Network:
             for entry in inp:
                 new.append([(1 / (1 + math.exp(-entry[0])))])
             return np.matrix(new)
+        elif self.activate_function == "relu":
+            # TO DO
+            print("Not yet implemented")
+            sys.exit()
 
     def propagate_forwards(self, inp):
         self.activation = [np.matrix(inp)]
-        print("\n\n", self.activation[-1], "\n\n",self.weights[0])
+        print("\n\n", self.activation[-1], "\n\n", self.weights[0])
         for layer in range(self.layer_number - 2):
             self.activation.append((self.activate(self.weights[layer] * self.activation[-1]) + self.bias[layer]))
 
@@ -81,7 +102,7 @@ class Network:
     def cost(self):
         cost = self.target - self.output
         cost = np.linalg.norm(cost)
-        cost = 1/2 * np.square(cost)
+        cost = 1 / 2 * np.square(cost)
         return cost
 
     def test(self, inp):
@@ -91,19 +112,11 @@ class Network:
         self.target = np.matrix(tar)
         self.output = self.propagate_forwards(inp)
         cost = self.cost()
-        print("\nFor Input: \n", np.matrix(inp), "\nAnd desired output: \n", self.target, "\nWe got: \n", self.output, "\nCost: \n", cost)
+        print("\nInput: \n", np.matrix(inp), "\nTarget Output: \n", self.target, "\nOutput: \n", self.output, "\nCost: \n", cost)
 
     def delta(self, layer):
         if layer == self.layer_number - 1:
             return np.multiply((self.activation[layer - 1] * (1 - self.activation[layer - 1])), (self.output - self.target))
         else:
             return np.multiply((self.activation[layer - 1] * (1 - self.activation[layer - 1])), self.weights[layer].transpose * self.delta(layer + 1))
-
-
-
-
-A = Network([(3),(2),(2)], "sigmoid")
-
-A.test_info([[3],[2],[2]], [[2],[2]])
-
 
