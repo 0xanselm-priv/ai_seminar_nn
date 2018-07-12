930082917e4f20b34fe24fed85170430e4bf0d8a
Niels Heissel <niels.heissel@konzepthaus-ws.de>
2018-07-12 10:28:58 +0200

    TEST SHit for you

--- neuralnetwork/simplenn/Test-Package/mnist_test6.py
+++ neuralnetwork/simplenn/Test-Package/mnist_test6.py
@@ -92,7 +92,7 @@ class Main():
         acc2 = []
 
         for ind in range(10000):
-            ind = random.randint(0, 55000)
+            ind = random.randint(0, 54999)
             percentage = percentage + 1 / 10000
             if ind % 250 == 0:
                 error1 = 0
